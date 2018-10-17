08bfda896efa1fddd4d6a6580caed20616deec05
Sun <30409932+Origintk@users.noreply.github.com>
2018-10-17 15:23:37 -0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,8 +1,8 @@
-local Tunnel = module("vrp", "lib/Tunnel")
-local Proxy = module("vrp", "lib/Proxy")
+--local Tunnel = module("vrp", "lib/Tunnel")
+--local Proxy = module("vrp", "lib/Proxy")
 
-vRP = Proxy.getInterface("vRP")
-vRPclient = Tunnel.getInterface("vRP")
+--vRP = Proxy.getInterface("vRP")
+--vRPclient = Tunnel.getInterface("vRP")
 --vRPcoresun = {}
 --Tunnel.bindInterface("vrp_core_sun_basico",vRPcoresun)
 
